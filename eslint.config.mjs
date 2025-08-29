26f4a900146588f71615f51ca86215b947948bec
sbush92 <sjbush92@gmail.com>
2025-08-28 21:15:50 -0600

    chore: update eslint.config.mjs for testing files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,23 @@ import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ["**/e2e-spec.js", "**/*.test.js", "**/*.spec.js"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.mocha,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  },
   tseslint.configs.recommended,
 ]);
