56be50191546fe236ddedeac2e6194cd517c9ee5
sbush92 <sjbush92@gmail.com>
2025-10-25 19:20:05 -0600

    feat: add db connection

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const db = require('./db/index');
+const PORT = 8080;
+
+app.use(express.json());
+
+app.get('/blog', async (req, res) => {
+  try {
+    const result = await db.query('SELECT * FROM blog_posts');
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.get('/test', (req, res) => {
+  res.status(200).send({
+    tshirt: 'white',
+    size: 'xxl'
+  });
+});
+
+app.post('/test/:id', (req, res) => {
+  const { id } = req.params;
+  const { logo } = req.body;
+
+  if (!logo) {
+    res.status(418).send({ message: 'We need a logo!' });
+    return;
+  }
+
+  res.send({
+    tshirt: `shirt with your ${logo} and ID of ${id}`,
+  });
+});
+
+app.listen(
+  PORT,
+  () => console.log(`Server running on port http://localhost:${PORT}`)
+);
